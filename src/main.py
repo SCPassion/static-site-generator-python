f4fa34b02e3b66867f398050e582f24994eebf36
SCPassion <bernard.cryptohk@gmail.com>
2025-08-04 08:48:16 +0200

    Remove index.html and styles.css files; update main.py to include a TextNode class and a main function for demonstration.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    text_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(text_node)
+
+if __name__ == "__main__":
+    main()
